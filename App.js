65e3dc43f1da23fe6eee35c63eb9cf4223bf497d
Ivan <75654991+ibarraz5@users.noreply.github.com>
2023-01-07 22:57:35 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, SER423! My name is Ivan</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00FF00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
